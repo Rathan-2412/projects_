5a3c5ab96fbd59102928d42c1e10d4ae386b7096
Rathan-2412 <rathanreddy452@gmail.com>
2025-01-06 09:58:10 +0530

    Your commit message

---
+++ index.js
@@ -0,0 +1,15 @@
+// Write your code below:
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const mainheading = document.getElementById('main-heading');
+mainheading.textContent = 'Fruit World';
+mainheading.style.color = "orange";
+
+const basketheading = document.getElementById('basket-heading');
+
+basketheading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
