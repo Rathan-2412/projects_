7c001fc3b2f1432988ef2d9e99b63bdd9af612a7
Rathan-2412 <rathanreddy452@gmail.com>
2025-03-14 09:08:07 +0530

    Initial commit

---
+++ books.js
@@ -0,0 +1,15 @@
+const express=require('express')
+
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+    console.log('GET /books called');
+    res.send("Here is the list of books!")
+})
+router.post('/',(req,res)=>{
+    const bookData = req.body;
+    console.log('Book data received:', bookData);
+    res.send("Book has been added!")
+})
+
+module.exports=router
